364550f55d7a40d30d34b5fafee3fb5558007d64
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-01-17 17:24:56 +0200

    - add fix imports of routes in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-console */
 import express from 'express';
-import routes from './server/routes/userRoutes';
+import routes from './server/Routes/userRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
