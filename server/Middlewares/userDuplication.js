221a1188ad45478630cc818c546dbd6bf7597b7e
Muhire-Josue <rutayisire.josue09@gmail.com>
2020-01-29 16:52:26 +0200

    feature(login): login using database
    - add feature login using postgres to store data
    - add tests
    - add validation
    [Finishes #170974632]

--- server/Middlewares/userDuplication.js
+++ server/Middlewares/userDuplication.js
@@ -2,7 +2,7 @@ import query from '../Helpers/userQuery';
 import response from '../Helpers/response';
 
 const userDuplication = async (req, res, next) => {
-  const duplicatedUser = await query.getByEmail(req.body.email);
+  const duplicatedUser = await query.findByEmail(req.body.email);
   if (duplicatedUser) {
     return response.failureResponse(res, 409, 'Email already exists');
   }
--- server/Middlewares/userFound.js
+++ server/Middlewares/userFound.js
@@ -1,8 +1,8 @@
 import query from '../Helpers/userQuery';
 import response from '../Helpers/response';
 
-const userFound = (req, res, next) => {
-  const user = query.findByEmail(req.body.email);
+const userFound = async (req, res, next) => {
+  const user = await query.findByEmail(req.body.email);
   if (!user) {
     return response.failureResponse(res, 404, 'User not found');
   }
